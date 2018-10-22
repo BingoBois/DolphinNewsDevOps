4c66775934b43bdbed5a3b4ea2ac221bea0440f7
Viterkim <viktorkim2005@hotmail.com>
2018-10-22 23:31:50 +0200

    added status site for building

--- update-listener/app.js
+++ update-listener/app.js
@@ -3,21 +3,29 @@ const createHandler = require('github-webhook-handler');
 const handler = createHandler({ path: '/webhook', secret: 'sutmig' });
 const updater = require('./updatecluster'); 
 
+let buildEvents = [`Waiting for work Time: ${new Date()}`];
+
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404
-    res.end('no such location')
+    let prettyMessage = `Building status: ${buildEvents[buildEvents.length-1]}\n\n\n`;
+    buildEvents.forEach((e) => {
+      prettyMessage += `${e}\n\n`;
+    });
+    res.end(prettyMessage)
   })
 }).listen(7777)
  
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+  buildEvents.push(`Error: ${err.message} Time: ${new Date()}`);
 })
  
 handler.on('push', async function (event) {
+  let repoName = event.payload.repository.name;
   if(event.payload.ref === "refs/heads/master"){
-    console.log('Running kub.sh');
+    buildEvents.push(`Started building ${repoName} Time: ${new Date()}`);
     await updater.updateCluster(event.payload.repository.name);
-    console.log('Done with kub.sh');
+    buildEvents.push(`Built ${repoName} done! Time: ${new Date()}`);
+    buildEvents.push(`Waiting for work Time: ${new Date()}`);
   }
 });
--- update-listener/kuber.sh
+++ update-listener/kuber.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 if [ $1 = "DolphinNewsFrontend" ]; then
-  docker build --no-cache -t $2 /root/devops/resource-manifests/images/frontend
-  docker push $2
-  kubectl apply -f /root/devops/resource-manifests/deployments/dolphin-frontend-deployment.yaml --record
+  echo "First thingy"
 else
-  docker build --no-cache -t $2 /root/devops/resource-manifests/images/backend
-  docker push $2
-  kubectl apply -f /root/devops/resource-manifests/deployments/dolphin-backend-deployment.yaml --record
+  echo "Other thingy"
 fi

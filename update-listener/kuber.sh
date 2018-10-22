de1642c7d02388fc7a56d0b3bf9bd4455915c784
Viterkim <viktorkim2005@hotmail.com>
2018-10-22 23:34:23 +0200

    Ooopsies

--- update-listener/kuber.sh
+++ update-listener/kuber.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 if [ $1 = "DolphinNewsFrontend" ]; then
-  echo "First thingy"
+  docker build --no-cache -t $2 /root/devops/resource-manifests/images/frontend
+  docker push $2
+  kubectl apply -f /root/devops/resource-manifests/deployments/dolphin-frontend-deployment.yaml --record
 else
-  echo "Other thingy"
+  docker build --no-cache -t $2 /root/devops/resource-manifests/images/backend
+  docker push $2
+  kubectl apply -f /root/devops/resource-manifests/deployments/dolphin-backend-deployment.yaml --record
 fi

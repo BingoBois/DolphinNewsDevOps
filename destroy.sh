c380bebcca57fc512d7a26431a374ebe97bb0848
Your Name <dolphin@vmi220173.contaboserver.net>
2018-11-10 18:09:11 +0100

    Prometheus + grafana

---
+++ destroy.sh
@@ -0,0 +1,5 @@
+kubectl delete --all services --namespace=monitoring
+kubectl delete --all deployments --namespace=monitoring
+kubectl delete --all daemonset --namespace=monitoring
+kubectl delete --all statefulset --namespace=monitoring
+kubectl delete --all pods --namespace=monitoring
